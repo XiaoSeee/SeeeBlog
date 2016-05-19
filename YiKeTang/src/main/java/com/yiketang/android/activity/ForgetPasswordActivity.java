2191adb65511f0993dfcbe51f94cae99fef79370
wuxiang <wuxiang@tinglibao.com.cn>
2016-05-19 20:11:56 +0800

    首次提交

---
+++ YiKeTang/src/main/java/com/yiketang/android/activity/ForgetPasswordActivity.java
@@ -0,0 +1,20 @@
+package com.yiketang.android.activity;
+
+import com.yiketang.android.R;
+import com.yiketang.android.base.ToolbarActivity;
+
+/**
+ * Created by WuXiang on 2016/5/19.
+ * ..
+ */
+public class ForgetPasswordActivity extends ToolbarActivity {
+    @Override
+    protected int getContentView() {
+        return R.layout.user_forget_password;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
--- YiKeTang/src/main/java/com/yiketang/android/activity/LoginActivity.java
+++ YiKeTang/src/main/java/com/yiketang/android/activity/LoginActivity.java
@@ -1,34 +1,89 @@
 package com.yiketang.android.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
 
 import com.yiketang.android.R;
+import com.yiketang.android.base.BaseActivity;
 
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends BaseActivity {
 
     // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
+    private TextInputLayout mEmailLayout;
+    private TextInputLayout mPasswordLayout;
+    private EditText mInputEmail;
+    private EditText mInputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initView();
+
+    }
+
+    private void initView() {
         // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
+        mEmailLayout = findView(R.id.email_layout);
+        mPasswordLayout = findView(R.id.password_layout);
+        mInputEmail = findView(R.id.input_phone);
+        mInputPassword = findView(R.id.input_password);
+
+        findView(R.id.sign_in_button).setOnClickListener(this);
+        findView(R.id.forget_password).setOnClickListener(this);
+        findView(R.id.user_register).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                submit();
+                break;
+            case R.id.forget_password:
+                Intent intent = new Intent(mContext, ForgetPasswordActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.user_register:
+                break;
+        }
+    }
+
+    private void submit() {
+        boolean isEmptyEmail = isEmptyEmail();
+        boolean isEmptyPassword = isEmptyPassword();
+        if (isEmptyEmail) {
+            mEmailLayout.setError(getString(R.string.phone_is_null));
+            mPasswordLayout.setError(null);
+        } else if (isEmptyPassword) {
+            mPasswordLayout.setError(getString(R.string.password_is_null));
+            mEmailLayout.setError(null);
+        } else {
+
+        }
+    }
+
+    private boolean isEmptyEmail() {
+        return mInputEmail.getText() == null
+                || mInputEmail.getText().toString().isEmpty();
+
+    }
+
+    private boolean isEmptyPassword() {
+        return mInputPassword.getText() == null
+                || mInputPassword.getText().toString().isEmpty();
+
     }
 
 }
